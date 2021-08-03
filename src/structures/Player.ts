f16f15b4ee57c6a13842a434d4a67a2aeb38387b
BR88C <br88c@br88c.dev>
2021-08-03 11:32:08 -0400

    Add Player#remove()

--- src/structures/Player.ts
+++ src/structures/Player.ts
@@ -477,6 +477,22 @@ export class Player extends EventEmitter<PlayerEvents> {
   }
 
   /**
+   * Remove a track from the queue.
+   * @param index The index of the track to remove.
+   * @param advanceQueue If the queue should advance if the removed track is the current track playing. If false, the player will be stopped.
+   * @returns The removed track.
+   */
+  public async remove (index: number, advanceQueue: boolean = true): Promise<Track | TrackPartial> {
+    if (!this.queue[index]) throw new Error('Invalid index')
+    const removedTrack = this.queue.splice(index, 1)[0]
+    if (index === this.queuePosition) {
+      if (advanceQueue) await this._advanceQueue()
+      else await this.stop()
+    }
+    return removedTrack
+  }
+
+  /**
    * Clear the queue.
    * @param stop If true, if a track is currently playing it will be stopped and removed from the queue. If false, if a track is playing it will be preserved.
    */
